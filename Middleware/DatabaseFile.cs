8471d35afe72005190cfd9a53da8717777c0156b
MrJackSpade <loffredo.ap@gmail.com>
2019-11-09 03:36:19 -0700

    Auto Commit

--- Middleware/DatabaseFile.cs
+++ Middleware/DatabaseFile.cs
@@ -17,6 +17,9 @@ using System.Threading.Tasks;
 namespace Penguin.Web.Errors.Middleware
 {
     //https://exceptionnotfound.net/using-middleware-to-log-requests-and-responses-in-asp-net-core/
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "<Pending>")]
     public class DatabaseFileServer : IPenguinMiddleware, IMessageHandler
     {
         public static ConcurrentDictionary<string, bool> ExistingFiles;
@@ -45,6 +48,106 @@ namespace Penguin.Web.Errors.Middleware
                 ExistingFiles.TryAdd(message.Target.FilePath, !message.Target.IsDeleted);
             }
         }
+
+
+        private static async Task RangeDownload(string fullpath, HttpContext context)
+        {
+            long size, start, end, length, fp = 0;
+            using (StreamReader reader = new StreamReader(fullpath))
+            {
+
+                size = reader.BaseStream.Length;
+                start = 0;
+                end = size - 1;
+                length = size;
+                // Now that we've gotten so far without errors we send the accept range header
+                /* At the moment we only support single ranges.
+                 * Multiple ranges requires some more work to ensure it works correctly
+                 * and comply with the spesifications: http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
+                 *
+                 * Multirange support annouces itself with:
+                 * header('Accept-Ranges: bytes');
+                 *
+                 * Multirange content must be sent with multipart/byteranges mediatype,
+                 * (mediatype = mimetype)
+                 * as well as a boundry header to indicate the various chunks of data.
+                 */
+                context.Response.Headers.Add("Accept-Ranges", "0-" + size);
+                // header('Accept-Ranges: bytes');
+                // multipart/byteranges
+                // http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
+
+                if (!String.IsNullOrEmpty(context.Request.Headers["Range"]))
+                {
+                    long anotherStart = start;
+                    long anotherEnd = end;
+                    string[] arr_split = context.Request.Headers["Range"].ToString().Split('=');
+                    string range = arr_split[1];
+
+                    // Make sure the client hasn't sent us a multibyte range
+                    if (range.IndexOf(",") > -1)
+                    {
+                        // (?) Shoud this be issued here, or should the first
+                        // range be used? Or should the header be ignored and
+                        // we output the whole content?
+                        context.Response.Headers.Add("Content-Range", "bytes " + start + "-" + end + "/" + size);
+                        context.Response.StatusCode = (int)HttpStatusCode.RequestedRangeNotSatisfiable;
+                        return;
+                    }
+
+                    // If the range starts with an '-' we start from the beginning
+                    // If not, we forward the file pointer
+                    // And make sure to get the end byte if spesified
+                    if (range.StartsWith("-"))
+                    {
+                        // The n-number of the last bytes is requested
+                        anotherStart = size - Convert.ToInt64(range.Substring(1));
+                    }
+                    else
+                    {
+                        arr_split = range.Split('-');
+                        anotherStart = Convert.ToInt64(arr_split[0]);
+                        long temp = 0;
+                        anotherEnd = (arr_split.Length > 1 && Int64.TryParse(arr_split[1].ToString(), out temp)) ? Convert.ToInt64(arr_split[1]) : size;
+                    }
+                    /* Check the range and make sure it's treated according to the specs.
+                     * http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
+                     */
+                    // End bytes can not be larger than $end.
+                    anotherEnd = (anotherEnd > end) ? end : anotherEnd;
+                    // Validate the requested range and return an error if it's not correct.
+                    if (anotherStart > anotherEnd || anotherStart > size - 1 || anotherEnd >= size)
+                    {
+
+                        context.Response.Headers.Add("Content-Range", "bytes " + start + "-" + end + "/" + size);
+                        context.Response.StatusCode = (int)HttpStatusCode.RequestedRangeNotSatisfiable;
+                        return;
+                    }
+                    start = anotherStart;
+                    end = anotherEnd;
+
+                    length = end - start + 1; // Calculate new content length
+                    fp = reader.BaseStream.Seek(start, SeekOrigin.Begin);
+                    context.Response.StatusCode = 206;
+                }
+            }
+            // Notify the client the byte range we'll be outputting
+            context.Response.Headers.Add("Content-Range", "bytes " + start + "-" + end + "/" + size);
+            context.Response.Headers.Add("Content-Length", length.ToString());
+
+            byte[] response = new byte[length];
+            using (BinaryReader reader = new BinaryReader(new FileStream(fullpath, FileMode.Open)))
+            {
+                reader.BaseStream.Seek(start, SeekOrigin.Begin);
+                reader.Read(response, 0, (int)length);
+            }
+
+            // Start buffered download
+            await context.Response.Body.WriteAsync(response, 0, response.Length);
+            await context.Response.Body.FlushAsync();
+
+        }
+
 
         public async Task Invoke(HttpContext context)
         {
@@ -92,23 +195,29 @@ namespace Penguin.Web.Errors.Middleware
             context.Response.StatusCode = (int)HttpStatusCode.OK;
             context.Response.ContentType = MimeType;
 
-            byte[] fileData;
-
-            if (databaseFile.Data.Length != 0)
+            if (String.IsNullOrEmpty(context.Request.Headers["Range"]))
             {
-                fileData = databaseFile.Data;
-            }
-            else
-            {
-                fileData = File.ReadAllBytes(databaseFile.FullName);
-            }
+                byte[] fileData;
 
-            context.Response.ContentLength = fileData.Length;
+                if (databaseFile.Data.Length != 0)
+                {
+                    fileData = databaseFile.Data;
+                }
+                else
+                {
+                    fileData = File.ReadAllBytes(databaseFile.FullName);
+                }
 
-            using (Stream stream = context.Response.Body)
+                context.Response.ContentLength = fileData.Length;
+
+                using (Stream stream = context.Response.Body)
+                {
+                    await stream.WriteAsync(fileData, 0, fileData.Length);
+                    await stream.FlushAsync();
+                }
+            } else
             {
-                await stream.WriteAsync(fileData, 0, fileData.Length);
-                await stream.FlushAsync();
+                await RangeDownload(databaseFile.FullName, context);
             }
         }
     }
